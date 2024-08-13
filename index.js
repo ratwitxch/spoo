ca3e37265048f76b27a2c4b0204cf352778fb516
˚ ༘♡ , bee <135217941+ratwitxch@users.noreply.github.com>
2024-08-13 13:27:08 +0700

    Random
    Just making some random stuff. My doggies' page

---
+++ index.js
@@ -0,0 +1,19 @@
+//console.log(`你好`);
+//console.log(`請離開吧。`);
+
+window.alert(`警告！私人伺服器。`);
+
+// mehh
+const washday = 'Sunday';
+
+document.getElementById("Riri").textContent = `牛奶狗`;
+document.getElementById("Riri-Type").textContent = `Type: Beagle`;
+document.getElementById("user-submit").onclick = function(){
+    username = document.getElementById("user-input").value;
+    console.log(`用戶名 : ${username}`);
+}
+
+
+//let username = window.prompt("用戶名");
+
+//console.log(`username = ${username}`);
